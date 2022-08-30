c448c1016c4ec7d95c71f7ca52098a22066d5509
Max Gasaro <massimilianogasaro@me.com>
2022-08-30 17:37:39 +0200

    istanzio oggetto studente1

--- CorsoCSharp/Program.cs
+++ CorsoCSharp/Program.cs
@@ -2,7 +2,10 @@
 // Con .NET6 possiamo anche solo scrivere il corpo del metodo Main
 // senza inserire tutto il codice della classe e il namespace
 
-Console.WriteLine("Hello, World!");
-Console.Read();
+using CorsoCSharp;
 
+//Console.WriteLine("Hello, World!");
+//Console.Read();
 
+Studente studente1 = new Studente("Matteo", "Rossi", 25, "matematica");
+studente1.Saluta();
--- CorsoCSharp/Studente.cs
+++ CorsoCSharp/Studente.cs
@@ -6,17 +6,22 @@ using System.Threading.Tasks;
 
 namespace CorsoCSharp
 {
+
+    // creo la classe studente ed estendo da persona 
     internal class Studente : Persona
     {
         private string _corso;
 
+        // nel costruttore è necessario inserire :base per potergli fare ereditare le proprietà
         public Studente(string name, string surname, int age, string corso) : base(name, surname, age)
         {
             _corso = corso;
         }
 
+        // con override sovrascrivo il metodo Saluta
         public override void Saluta()
         {
+            base.Saluta();
             Console.WriteLine($"Ciao sono uno studente e frequento il corso di {this._corso}");
         }
     }
