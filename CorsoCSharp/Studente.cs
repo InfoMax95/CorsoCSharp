68f16b6f039437ebff308ef0e0fa9fd7a2700228
Max Gasaro <massimilianogasaro@me.com>
2022-08-30 17:29:54 +0200

    create class studente

--- CorsoCSharp/Studente.cs
+++ CorsoCSharp/Studente.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CorsoCSharp
 {
-    internal class Studente
+    internal class Studente : Persona
     {
+        private string _corso;
+
+        public Studente(string name, string surname, int age, string corso) : base(name, surname, age)
+        {
+            _corso = corso;
+        }
+
+        public override void Saluta()
+        {
+            Console.WriteLine($"Ciao sono uno studente e frequento il corso di {this._corso}");
+        }
     }
 }
