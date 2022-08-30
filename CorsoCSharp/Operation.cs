e17376c6851f05bfc63a8850af0c898fc3987bee
Max Gasaro <massimilianogasaro@me.com>
2022-08-30 09:01:49 +0200

    Add project files.

---
+++ CorsoCSharp/Operation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoCSharp
+{
+    internal class Operation
+    {
+        public void Sum(int[] numbers)
+        {
+
+        }
+    }
+}
---
+++ CorsoCSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Con .NET6 possiamo anche solo scrivere il corpo del metodo Main
+// senza inserire tutto il codice della classe e il namespace
+
+Console.WriteLine("Hello, World!");
+int v = Console.Read();
+
+
